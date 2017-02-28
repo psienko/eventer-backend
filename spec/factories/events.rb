1bf49cb4b374278a4f817d483ba648c59222568a
Przemek <sienkowskip@gmail.com>
2017-02-28 21:26:12 +0100

    Added events table. Added factory girl.

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :event do
+    sequence(:name) { |n| "weeding #{n}" }
+    type 'weeding'
+    start_at DateTime.now + 1.day
+    end_at DateTime.now + 2.day
+    place 'Warsaw'
+    status 'new'
+  end
+end
