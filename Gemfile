9bdad61a209e43362f965372b96e98977e638454
Przemek <sienkowskip@gmail.com>
2017-03-08 18:22:56 +0100

    Added shoulda-matchers and database-cleaner gems

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ group :development, :test do
   # gem 'byebug', platform: :mri
   gem 'pry', '~> 0.10.4'
   gem 'rspec-rails', '~> 3.5.2'
-  gem 'factory_girl', '~> 4.5'
 end
 
 group :development do
@@ -33,6 +32,12 @@ group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'factory_girl', '~> 4.5'
+  gem 'shoulda-matchers', '~> 3.1', '>= 3.1.1'
+  gem 'database_cleaner', '~> 1.5', '>= 1.5.3'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,7 +54,33 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace("gem name")  
+end
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configuration do |config|
+  # add `FactoryGirl` methods
+  config.include FactoryGirl::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 FactoryGirl.find_definitions
