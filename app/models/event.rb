5de2fb458efa23ec4088479bc8d89d63ae5a7462
Przemek <sienkowskip@gmail.com>
2017-03-13 20:59:03 +0100

    Fixed identations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,23 +1,22 @@
 class Event < ApplicationRecord
+  AVAILABLE_STATUSES = %w(new ongoing finished)
 
-	AVAILABLE_STATUSES = %w(new ongoing finished)
+  validates_datetime :end_at, on_or_after: :start_at, on_or_after_message: 'can not be before start date'
+  validates :name, :place, length: { maximum: 250 }, presence: true
+  validates :event_type, length: { maximum: 50 }, presence: true
+  validates :status, inclusion: { in: AVAILABLE_STATUSES }
 
-	validates_datetime :end_at, on_or_after: :start_at, on_or_after_message: 'can not be before start date'
-	validates :name, :place, length: { maximum: 250 }, presence: true
-	validates :event_type, length: { maximum: 50 }, presence: true
-	validates :status, inclusion: { in: AVAILABLE_STATUSES }
+  validate :past_start_at
 
-	validate :past_start_at
-
-	def finished?
-		status.to_sym == :finished
-	end
+  def finished?
+    status.to_sym == :finished
+  end
 
 
-	private
+  private
 
-	def past_start_at
-		return true if start_at.present? && start_at > DateTime.now || finished? 
-		errors.add(:start_at, 'can not be past')
-	end
+  def past_start_at
+    return true if start_at.present? && start_at > DateTime.now || finished? 
+    errors.add(:start_at, 'can not be past')
+  end
 end
--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,13 +1,13 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
   attributes :id, :event_type, :name, :start_at, :end_at,
-  					 :place, :status, :created_at, :updated_at,
-  					 :errors, :error_messages
+             :place, :status, :created_at, :updated_at,
+             :errors, :error_messages
 
   def errors
-  	object.errors.messages if object.errors.present?
+    object.errors.messages if object.errors.present?
   end
 
   def error_messages
-  	object.errors.full_messages
+    object.errors.full_messages
   end
 end
