f113526f36bf92f5fd82b3818a2b6f37c1d50431
Przemek <sienkowskip@gmail.com>
2017-03-13 20:24:45 +0100

    Added update endpoint to events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,33 +1,38 @@
 module Api
-	module V1
-		class EventsController < ApiController
-			before_action :set_event, only: [:show]
+  module V1
+    class EventsController < ApiController
+      before_action :set_event, only: [:show, :update]
 
-			def index
-				@events = Event.all
-				render json: @events
-			end
+      def index
+        @events = Event.all
+        render json: @events
+      end
 
-			def show
-				render json: @event
-			end
+      def show
+        render json: @event
+      end
 
-			def create
-				@event = Event.new(event_params)
-				status = @event.save ? :created : :unprocessable_entity
-				render json: @event, status: status
-			end
+      def create
+        @event = Event.new(event_params)
+        status = @event.save ? :created : :unprocessable_entity
+        render json: @event, status: status
+      end
 
-			private
+      def update
+        status = @event.update(event_params) ? :ok : :unprocessable_entity
+        render json: @event, status: status
+      end
 
-			def set_event
-				@event = Event.find(params[:id])
-			end
+      private
 
-			def event_params
-				params = ActionController::Parameters.new(json)
-    		params.permit(:event_type, :name, :start_at, :end_at, :place, :status)
-			end
-		end
-	end
+      def set_event
+        @event = Event.find(params[:id])
+      end
+
+      def event_params
+        params = ActionController::Parameters.new(json)
+        params.permit(:event_type, :name, :start_at, :end_at, :place, :status)
+      end
+    end
+  end
 end
--- spec/requests/api/v1/events_controller_spec.rb
+++ spec/requests/api/v1/events_controller_spec.rb
@@ -1,113 +1,164 @@
 require 'rails_helper'
 
 RSpec.describe 'Events API/V1', type: :request do
-	describe 'GET /events/:id' do
-		context 'when event exist' do
-			before do
-				@event = create :event
-				get "/api/v1/events/#{@event.id}"
-			end
+  describe 'GET /events/:id' do
+    context 'when event exist' do
+      before do
+        @event = create :event
+        get "/api/v1/events/#{@event.id}"
+      end
 
-			it 'responds with 200 HTTP status code' do
-				expect(response).to have_http_status 200
-			end
+      it 'responds with 200 HTTP status code' do
+        expect(response).to have_http_status 200
+      end
 
-			it 'responds with not empty json' do
-				expect(json).to_not be_empty
-				expect(json.size).to eql 9
-			end
+      it 'responds with not empty json' do
+        expect(json).to_not be_empty
+        expect(json.size).to eql 11
+      end
 
-		end
+    end
 
-		context 'when event does not exist' do
-			before { get "/api/v1/events/100" }
+    context 'when event does not exist' do
+      before { get "/api/v1/events/100" }
 
-			it 'responds with 404 HTTP status code' do
-				expect(response).to have_http_status 404
-			end
-		end
-	end
+      it 'responds with 404 HTTP status code' do
+        expect(response).to have_http_status 404
+      end
+    end
+  end
 
-	describe 'GET /events' do
-		context 'when events exist' do
-			before do
-				@event1 = create :event
-				@event2 = create :event
-				get "/api/v1/events"
-			end
+  describe 'GET /events' do
+    context 'when events exist' do
+      before do
+        @event1 = create :event
+        @event2 = create :event
+        get "/api/v1/events"
+      end
 
-			it 'responds with 200 HTTP status code' do
-				expect(response).to have_http_status 200
-			end
+      it 'responds with 200 HTTP status code' do
+        expect(response).to have_http_status 200
+      end
 
-			it 'responds with all events' do
-				expect(json.size).to eql 2
-			end
+      it 'responds with all events' do
+        expect(json.size).to eql 2
+      end
 
-		end
+    end
 
-		context 'when events does not exist' do
-			before { get "/api/v1/events" }
+    context 'when events does not exist' do
+      before { get "/api/v1/events" }
 
-			it 'responds with 200 HTTP status code' do
-				expect(response).to have_http_status 200
-			end
+      it 'responds with 200 HTTP status code' do
+        expect(response).to have_http_status 200
+      end
 
-			it 'responds with empty list of events' do
-				expect(json).to be_empty 
-			end
-		end
-	end
+      it 'responds with empty list of events' do
+        expect(json).to be_empty 
+      end
+    end
+  end
 
-	describe 'POST /events' do
-		let(:valid_parameters) do
-			{
-				eventType: 'event type', name: 'Event name', startAt: (DateTime.now + 1.day).to_s,
-				endAt: (DateTime.now + 2.day).to_s, place: 'my place', status: 'new'
-			}.to_json
-		end
+  describe 'POST /events' do
+    let(:valid_parameters) do
+      {
+        eventType: 'event type', name: 'Event name', startAt: (DateTime.now + 1.day).to_s,
+        endAt: (DateTime.now + 2.day).to_s, place: 'my place', status: 'new'
+      }.to_json
+    end
 
-		let(:invalid_parameters) do
-			{
-				eventType: 'event type', name: nil, startAt: '2000-01-02T08:00:00.000Z',
-				endAt: '2000-01-01T08:00:00.000Z', place: 'my place', status: 'new'
-			}.to_json
-		end
+    let(:invalid_parameters) do
+      {
+        eventType: 'event type', name: nil, startAt: '2000-01-02T08:00:00.000Z',
+        endAt: '2000-01-01T08:00:00.000Z', place: 'my place', status: 'new'
+      }.to_json
+    end
 
-		context 'when valid parameters' do
-			it 'responds with 201 HTTP status code' do
-				post '/api/v1/events', params: valid_parameters
-				expect(response).to have_http_status(201)
-			end
+    context 'when valid parameters' do
+      it 'responds with 201 HTTP status code' do
+        post '/api/v1/events', params: valid_parameters
+        expect(response).to have_http_status(201)
+      end
 
-			it 'creates event' do
-				expect do
-					post '/api/v1/events', params: valid_parameters
-				end.to change { Event.count }.by 1 
-			end
+      it 'creates event' do
+        expect do
+          post '/api/v1/events', params: valid_parameters
+        end.to change { Event.count }.by 1 
+      end
 
-			it 'responds with json' do
-				post '/api/v1/events', params: valid_parameters
-				expect(json[:name]).to eql 'Event name'
-			end
-		end
+      it 'responds with json' do
+        post '/api/v1/events', params: valid_parameters
+        expect(json[:name]).to eql 'Event name'
+      end
+    end
 
-		context 'when valid parameters' do
-			it 'responds with 422 HTTP status code' do
-				post '/api/v1/events', params: invalid_parameters
-				expect(response).to have_http_status(422)
-			end
+    context 'when valid parameters' do
+      it 'responds with 422 HTTP status code' do
+        post '/api/v1/events', params: invalid_parameters
+        expect(response).to have_http_status(422)
+      end
 
-			it 'does not create event' do
-				expect do
-					post '/api/v1/events', params: invalid_parameters
-				end.to change { Event.count }.by 0 
-			end
+      it 'does not create event' do
+        expect do
+          post '/api/v1/events', params: invalid_parameters
+        end.to change { Event.count }.by 0 
+      end
 
-			it 'responds with errors inside json' do
-				post '/api/v1/events', params: invalid_parameters
-				expect(json[:errors]).to_not be_empty
-			end
-		end
-	end
+      it 'responds with errors inside json' do
+        post '/api/v1/events', params: invalid_parameters
+        expect(json[:errors]).to_not be_empty
+      end
+    end
+  end
+
+  describe 'PUT /events/:id' do
+    let(:event) { create :event, name: 'my name' }
+
+    let(:event_valid_attrs) do
+      event.attributes.with_indifferent_access.merge(name: 'changed name').to_json
+    end
+
+    let(:event_invalid_attrs) do
+      event.attributes.with_indifferent_access.merge(name: nil).to_json
+    end
+
+    context 'when valid parameters' do
+      before do
+        put '/api/v1/events/' + event.id.to_s, params: event_valid_attrs
+      end
+
+      it 'updates event' do
+        expect(event.reload.name).to eql 'changed name'
+      end
+
+      it 'responds with 200 HTTP status code' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'responds with not empty json' do
+        expect(json).to_not be_empty
+        expect(json.size).to eql 11
+      end
+    end
+
+    context 'when invalid parameters' do
+      before do
+        put '/api/v1/events/' + event.id.to_s, params: event_invalid_attrs
+      end
+
+      it 'does not update event' do
+        expect(event.reload.name).to eql 'my name'
+      end
+
+      it 'responds with 422 HTTP status code' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'responds with not empty json' do
+        expect(json).to_not be_empty
+        expect(json.size).to eql 11
+      end
+    end
+
+  end
 end
